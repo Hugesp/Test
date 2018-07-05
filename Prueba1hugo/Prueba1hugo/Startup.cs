9785e90731d7affca55528d0f287c48ad9a72821
Ciro Mendoza Eumaña <ciro_d_unit@hotmail.com>
2018-07-05 15:07:21 -0500

    Proyecto

---
+++ Prueba1hugo/Prueba1hugo/Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(Prueba1hugo.Startup))]
+namespace Prueba1hugo
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
